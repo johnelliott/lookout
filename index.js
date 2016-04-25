9a7e1673622b9b319b7b8839a6ea2b3f8426f188
John Elliott <johnelliott703@gmail.com>
2016-04-25 16:09:18 -0400

    Make spoken sentence more informative, accurate

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fetch = require('isomorphic-fetch')
-//var querystring = require('querystring')
 var url = require('url')
+var cp = require('child_process')
 var debug = require('debug')('lookout')
 
 var token = process.argv[2]
@@ -16,44 +16,91 @@ var config = {
 
 var now = Math.floor(new Date().valueOf()/1000)
 debug('now', now)
-var startTime = now - 60*1000
+var startTime = now - 60*60
 var endTime = now
+// temporarily do just today from midnight to check against dashbord default total
+//var startTime = Math.floor(new Date(2016, 3, 25).valueOf()/1000)
+//var endTime = Math.floor(new Date().valueOf()/1000)
+
 
 var measurementPointId = 7423
 
-var getMessage = function(data) {
-  return [data.length, 'pedestrians in the last hour']
-}
-
-var resource = url.format({
+var dataUrl = url.format({
   hostname: 'api.placemeter.net/api/v1/measurementpoints',
   pathname: `/${measurementPointId}/data`,
   query: {
     start: startTime,
-    resolution: 'hour',
+    //resolution: 'minute', // don't need this...
+		//classes: 'all', // TODO find out why this doesn't work with 'ped'
     end: endTime
   }
 })
-debug(resource)
+debug('dataUrl', dataUrl)
 
-fetch(resource, config)
+var metadataUrl = url.format({
+	hostname: 'api.placemeter.net/api/v1/measurementpoints',
+	pathname: `/${measurementPointId}`
+})
+debug('metadataUrl', metadataUrl)
+
+var metadata = fetch(metadataUrl, config)
 .then(function(response) {
-  debug(response.headers.get('Content-Type'))
-  debug(response.headers.get('Date'))
-  debug(response.status)
-  debug(response.statusText)
-  debug(response.url)
-  // TODO replace with better error handling
-  if (response.statusText === "UNAUTHORIZED") process.exit(1)
-    return response
+	debug(response.headers.get('Content-Type'))
+	debug(response.headers.get('Date'))
+	debug(response.status)
+	debug(response.statusText)
+	debug(response.url)
+	// TODO replace with better error handling
+	if (response.statusText === "UNAUTHORIZED") process.exit(1)
+		return response
 })
 .then(function(res) {
-  return res.json();
+	return res.json()
 })
-.then(function(json) {
-  json.data.forEach((e)=>{debug(e)});
-  require('child_process').spawn('say', getMessage(json.data))
-});
+
+var pointData = fetch(dataUrl, config)
+	.then(function(response) {
+		debug(response.headers.get('Content-Type'))
+		debug(response.headers.get('Date'))
+		debug(response.status)
+		debug(response.statusText)
+		debug(response.url)
+		// TODO replace with better error handling
+		if (response.statusText === "UNAUTHORIZED") process.exit(1)
+			return response
+	})
+	.then(function(res) {
+		return res.json()
+	})
+
+	Promise.all([metadata, pointData]).then(function(values) {
+		// Placemeter has non-overlapping keys for these two endpoints, so merge data
+		var point = Object.assign({}, ...values)
+
+		var directionalCounts = point.data.map((e)=>{
+			return e.all
+		})
+//debug('directionalCounts', directionalCounts)
+
+		var sampleBiDirectionalSums = directionalCounts.map((s)=>{
+			var keys = Object.keys(s)
+			return keys.reduce((p, c)=>{
+				return s[p] + s[c]
+			})
+		})
+
+		var count = sampleBiDirectionalSums.reduce((p, c)=>{
+			return p+c
+		})
+		debug('count', count)
+
+
+		speak(`${point.name} had ${count} pedestrians in the last hour`)
+	})
+
+function speak(message) {
+  return cp.spawn('say', [message])
+}
 
 /*
  * Mac OS-X UI:
